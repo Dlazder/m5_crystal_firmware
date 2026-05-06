c56686c0fbd46e795d2967d5ebaf455c82a0159e
Dlazder <dlazder51@gmail.com>
2026-05-06 20:40:00 +0300

    fix: remove awful japan fonts from settings, font2 lefted just for example

--- src/system/globals.h
+++ src/system/globals.h
@@ -62,33 +62,15 @@ int BIG_TEXT = 4;
 int currentFontIndex = 0;
 
 const lgfx::IFont* systemFonts[] = {
-  &fonts::Font0,          // Basic 8x8
-  &fonts::lgfxJapanMincho_8,
-  &fonts::lgfxJapanMinchoP_8,
-  &fonts::lgfxJapanGothic_8,
-  &fonts::lgfxJapanGothicP_8,
-  // &fonts::Font2,          // 8x16
-  // &fonts::Font4,          // 8x16
-  // &fonts::Font6,          // 24x32
-  // &fonts::Font7,          // 24x48
-  // &fonts::Font8,          // 8x16
-  // &fonts::AsciiFont8x16,  // Classic 8x16
-  // &fonts::AsciiFont24x48  // Classic 24x48
+  &fonts::Font0,          // Basic 8x8 ASCII
+  // &u8g2FontUnicode,       // Terminus 11px Unicode (Cyrillic, Latin Extended)
+  &fonts::Font2,          // 8x16
 };
 
 const char* fontNames[] = {
   "Default (Font0)",
-  "JapanMincho_8",
-  "JapanMinchoP_8",
-  "JapanGothic_8",
-  "JapanGothicP_8"
-  // "Font2 (8x16)",
-  // "Font4 (8x16 bold)",
-  // "Font6 (24x32)",
-  // "Font7 (24x48)",
-  // "Font8 (8x16 thin)",
-  // "Ascii 8x16",
-  // "Ascii 24x48"
+  // "Terminus 11px",
+  "Font2 (8x16)",
 };
 
 int brightnessMax = 255;
