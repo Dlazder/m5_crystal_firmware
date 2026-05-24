ad57882c4ededa3c6d9aef17abf95f836607eb59
Dlazder <dlazder51@gmail.com>
2026-05-24 13:08:49 +0300

    feat: update volume settings ui

--- src/functions/settings/volumeLoop.h
+++ src/functions/settings/volumeLoop.h
@@ -1,29 +1,44 @@
 // PID::VOLUME
 
+void _volumeDraw() {
+	static const int barHeight = 20;
+	int visibleHeight = canvas.height() - getStatusBarOffset();
+	int centerY = visibleHeight / 2 - barHeight / 2;
+	canvas.clear();
+	drawProgressBar(volume / volumeDividor, volumeStep, centerY);
+	drawHintRange();
+}
+
 void volumeLoop() {
 	if (isSetup()) {
-		String lines[] = {
-			String(L->TXT_SETTINGS_VOLUME) + String(volume / volumeDividor) + "/10",
-			L->TXT_PRESS_A
-		};
-		centeredPrintRows(lines, 2, MEDIUM_TEXT);
+		_volumeDraw();
 		updateTimer();
 	}
 
-	if ((isBtnAWasPressed() || isKbEnterPressed()) && checkTimer(100)) {
-		volume += volumeDividor;
-		if (volume > volumeMax) volume = volumeMin;
+	if ((isBtnAWasPressed() || isKbRightPressed()) && checkTimer(100)) {
+		volume = ((volume / volumeDividor) % volumeStep + 1) * volumeDividor;
 		DEVICE.Speaker.setVolume(volume);
 		soundBeep();
 		setData("volume", volume);
-
 		Serial.printf("Volume: %d\n", volume / volumeDividor);
-
-		String lines[] = {
-			String(L->TXT_SETTINGS_VOLUME) + String(volume / volumeDividor) + "/10",
-			L->TXT_PRESS_A
-		};
-		centeredPrintRows(lines, 2, MEDIUM_TEXT);
+		_volumeDraw();
 	}
+
+	if ((isBtnPWRWasPressed() || isKbLeftPressed()) && checkTimer(100)) {
+		int step = volume / volumeDividor;
+		volume = (step <= 1 ? volumeStep : step - 1) * volumeDividor;
+		DEVICE.Speaker.setVolume(volume);
+		soundBeep();
+		setData("volume", volume);
+		Serial.printf("Volume: %d\n", volume / volumeDividor);
+		_volumeDraw();
+	}
+
 	checkExit();
+
+	if (isWebDataRequested()) {
+		char text[50];
+		sprintf(text, "volume: %d", volume / volumeDividor);
+		webData = generateWebData("function", generateFunctionElement(text, MEDIUM_TEXT, "center"));
+	}
 }
